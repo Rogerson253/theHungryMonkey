722cf9bf0d6ed224f641c2c42c8e95b4bac7e381
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-03-14 17:30:24 -0500

    made an ajax call for yelp

--- apps.js
+++ apps.js
@@ -1,38 +1,55 @@
 $( document ).ready(function() {
 console.log("ready!");
 
-function getRecipes() {
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
+// function getRecipes() {
+//     var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    // We store all of the retrieved data inside of an object called "response"
-    .then(function(response) {
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
+//     // We store all of the retrieved data inside of an object called "response"
+//     .then(function(response) {
         
-        console.log(response);
-        console.log(queryURL);
+//         console.log(response);
+//         console.log(queryURL);
 
-    });
-}
+//     });
+// }
 
-getRecipes();
-function getRestaurants() {
-    var queryURL = "https://www.google.com/maps/embed/v1/place?key=AIzaSyC_zrOsFx0s4YZLCAiI5i1cp_6J7z0Did8&q=burger";
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    // We store all of the retrieved data inside of an object called "response"
-    .then(function(response) {
+// getRecipes();
+// function getRestaurants() {
+//     var queryURL = "https://www.google.com/maps/embed/v1/place?key=AIzaSyC_zrOsFx0s4YZLCAiI5i1cp_6J7z0Did8&q=burger";
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
+//     // We store all of the retrieved data inside of an object called "response"
+//     .then(function(response) {
         
-        console.log(response);
-        console.log(queryURL);
+//         console.log(response);
+//         console.log(queryURL);
 
-    });
-}
-getRestaurants();
+//     });
+// }
+// getRestaurants();
 
+
+
+    var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=nashville";
+   function yelpCall() {
+    $.ajax({
+        url: yelpURL,
+        method: "GET",
+        headers: {
+            "Authorization": "Bearer FX7G_LW66z7oBdEKO1pNijgUXQbOknj073l6OGxkmIZ1XWT7J2isalDcZmqv0UC0CM0yj3Mgqkqs-STQDSeOtL-C_RWLhzMbFdV1xdFV5RCmWXPWZz81cMkThT1tXnYx",
+        }
+    })
+
+        .then(function(response){
+            console.log(response);
+        })
+    }
+    yelpCall();
 //dont delete this dummy
 });
