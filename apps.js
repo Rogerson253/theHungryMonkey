660309cd5dd0f525a72c7c576ccf44c293e2f157
sammiefrog <sammiefrog@live.com>
2020-03-15 13:55:45 -0500

    two working apis

--- apps.js
+++ apps.js
@@ -1,39 +1,23 @@
 $( document ).ready(function() {
 console.log("ready!");
 
-// function getRecipes() {
-//     var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
+function getRecipes() {
+    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   })
-//     // We store all of the retrieved data inside of an object called "response"
-//     .then(function(response) {
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
         
-//         console.log(response);
-//         console.log(queryURL);
+        console.log(response);
+        console.log(queryURL);
 
-//     });
-// }
+    });
+}
 
-// getRecipes();
-// function getRestaurants() {
-//     var queryURL = "https://www.google.com/maps/embed/v1/place?key=AIzaSyC_zrOsFx0s4YZLCAiI5i1cp_6J7z0Did8&q=burger";
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   })
-//     // We store all of the retrieved data inside of an object called "response"
-//     .then(function(response) {
-        
-//         console.log(response);
-//         console.log(queryURL);
-
-//     });
-// }
-// getRestaurants();
-
+getRecipes();
 
 
     var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=nashville";
