f555c746126285719fe1df3413f42ba8fa9e6e7f
sammiefrog <sammiefrog@live.com>
2020-03-18 10:59:41 -0500

    reorganized html, changed search bar

--- apps.js
+++ apps.js
@@ -16,6 +16,10 @@ $.ajax({
 
         for(var j=0; j < 5; j++) {
             console.log(recipeData.meals[j].strMeal);
+            console.log(recipeData.meals[j].strMeal);
+            console.log(recipeData.meals[j].strMeal);
+            console.log(recipeData.meals[j].strMeal);
+            console.log(recipeData.meals[j].strMeal);
         }
 
     });
